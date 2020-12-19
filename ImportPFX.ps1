6512867f69329e975d502928e68bc84f9a745b03
Aeaddy <aeaddy@gmail.com>
2020-12-19 13:48:16 -0500

    Add files via upload

---
+++ ImportPFX.ps1
@@ -0,0 +1,8 @@
+﻿$RCP = "\\hyperv1\Media\source files\Certificates\wildcardServerAuth.pfx"
+$LCP = "$env:TEMP\wildcardServerAuth.pfx"
+$testPath = (Test-Path $LCP)
+if ($testPath -eq $false) { Copy-Item $RCP $LCP -Force }
+$A1 = [string][char[]][int[]]("80.97.115.115.119.111.114.100.49.50.51".Split(".")) -replace " "
+$A2 = ConvertTo-SecureString -String $A1 -AsPlainText -Force
+Import-PFXCertificate -FilePath $LCP -CertStoreLocation Cert:\LocalMachine\My -Exportable -Password $A2
+
---
+++ ImportSSLCertToIIS.ps1
@@ -0,0 +1,13 @@
+﻿$templateName = 'JamWebCert'
+$cert = Get-ChildItem 'Cert:\LocalMachine\My' | Where-Object{ $_.Extensions | Where-Object{ ($_.Oid.FriendlyName -eq 'Certificate Template Information') -and ($_.Format(0) -match $templateName) }}
+#$thumbprint = $cert.Thumbprint
+$iisSite = "WSUS Administration"
+
+$sysinfo = Get-WmiObject -Class Win32_ComputerSystem
+$server = “{0}.{1}” -f $sysinfo.Name, $sysinfo.Domain
+
+New-WebBinding -name $iisSite -Protocol https -HostHeader $server -Port 8531 -SslFlags 1
+$binding = Get-WebBinding -Name $iisSite -Protocol https  
+if ($binding) {
+    $binding.AddSslCertificate($cert.GetCertHashString(), "my")
+}
